b57d6de8388859db104f37dc71f6904c8fd4f02a
arkia <arkia@DESKTOP-9D90DSK>
2022-04-28 18:52:03 -0700

    Add project files.

---
+++ Homework1-Edwards/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Homework1_Edwards.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Homework1_Edwards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult SendForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SentForm(CardForm cardForm)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("SentForm", cardForm);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Homework1-Edwards/Models/CardForm.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Homework1_Edwards.Models
+{
+    public class CardForm
+    {
+        [Required(ErrorMessage = "Please enter your From address")]
+        public string From { get; set; }
+        [Required(ErrorMessage = "Please enter your To address")]
+        public string To { get; set; }
+        [Required(ErrorMessage = "Please enter your message")]
+        public string Message { get; set; }
+    }
+}
