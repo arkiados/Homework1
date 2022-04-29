4556fde531db87ab8fd95a0338e8cf8e0ea20c1e
David Edwards <arkiados@bellsouth.net>
2022-04-29 14:19:31 -0700

    initial commit

--- Homework1-Edwards/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Homework1_Edwards.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
